697b41f0d40d0d1f287a68c801a9b489d3d7167c
raulhn <raulhnpacheco@hotmail.com>
2026-08-17 21:10:43 +0200

    Se añade url base

--- boda/src/main.jsx
+++ boda/src/main.jsx
@@ -5,14 +5,15 @@ import App from "./App.jsx";
 import Retos from "./components/componentesRetos/Retos.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import ComponenteLogin from "./components/componenteLogin/ComponenteLogin.jsx";
+import { URL_BASE } from "./constantes.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<App />} />
-        <Route path={"/login"} element={<ComponenteLogin />} />
-        <Route path={"/reto"} element={<Retos />} />
+        <Route path={URL_BASE + "/"} element={<App />} />
+        <Route path={URL_BASE + "/login"} element={<ComponenteLogin />} />
+        <Route path={URL_BASE + "/reto"} element={<Retos />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
