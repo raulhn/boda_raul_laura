c928a35fc17d46b1a837453fdc3d087cfa9f1cf7
raulhn <raulhnpacheco@hotmail.com>
2026-08-17 23:17:33 +0200

    Se modifica la url al backend

--- boda/src/constantes.js
+++ boda/src/constantes.js
@@ -1,1 +1,2 @@
 export const URL_BASE = "/boda";
+export const URL_API = "/api_boda";
--- boda/src/servicios/serviceUsuario.js
+++ boda/src/servicios/serviceUsuario.js
@@ -1,8 +1,9 @@
 import { peticionServicio } from "./serviceComun.js";
+import { URL_API } from "../config.js";
 
 export async function loginUsuario(usuario, contrasena) {
   try {
-    const url = "/api/login";
+    const url = URL_API + "/login";
     const body = { usuario, contrasena };
     const respuesta = await peticionServicio("POST", url, body);
     return respuesta;
